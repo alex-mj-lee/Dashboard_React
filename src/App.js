ffdc11e984ef27d4b4d4bba29a63508b1dc55c54
Alex_MJ <alex.lee6200@gmail.com>
2022-11-08 15:55:52 -0500

    piechart

--- src/App.js
+++ src/App.js
@@ -27,9 +27,15 @@ import "./App.css";
 import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const {
+    activeMenu,
+    themeSettings,
+    setThemeSettings,
+    currentColor,
+    currentMode,
+  } = useStateContext();
   return (
-    <div>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
@@ -37,7 +43,8 @@ const App = () => {
               <button
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: "50%" }}
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
               >
                 <FiSettings />
               </button>
@@ -54,15 +61,15 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? "md:ml-72" : "flex-2"
-            }`}
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+            ${activeMenu ? "md:ml-72" : "flex-2"}`}
           >
             <div className="fixed md:static mg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
 
             <div>
+              {themeSettings && <ThemeSettings />}
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  SplineAreaSeries,
+  DateTime,
+  Legend,
+  Category,
+  Tooltip,
+  DataLabel,
+  BarSeries,
+} from "@syncfusion/ej2-react-charts";
 
+import { Header } from "../../components";
+import {
+  barChartData,
+  barPrimaryYAxis,
+  barPrimaryXAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../contexts/ContextProvider";
 const Bar = () => {
-  return <div>Bar</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounder-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <ChartComponent
+        id="bar-chart"
+        height="420px"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        legendSettings={{ background: "white" }}
+      >
+        <Inject services={[BarSeries, Legend, Tooltip, DataLabel, Category]} />
+        <SeriesCollectionDirective>
+          {barChartData.map((item, index) => (
+            <SeriesDirective
+              key={index}
+              dataSource={item}
+              xName="x"
+              yName="y"
+              type="Bar"
+            />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Bar;
